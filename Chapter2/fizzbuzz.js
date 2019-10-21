d0ed5277c3bedd90408db7ed56cf432028c0b0f2
Avdyukov Ivan <omnixejiharu@gmail.com>
2019-10-21 19:59:58 +0300

    FizzBuzz interview question

--- Chapter2/fizzbuzz.js
+++ Chapter2/fizzbuzz.js
@@ -1,2 +1,2 @@
-for (let i = 0; i < 100; )
-  console.log((++i % 3 ? "" : "fizz") + (i % 5 ? "" : "buzz") || i);
+for (let n = 0; n < 100; )
+  console.log((++n % 3 ? "" : "fizz") + (n % 5 ? "" : "buzz") || n);
